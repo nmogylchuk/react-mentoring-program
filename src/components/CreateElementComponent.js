00770dc27b41b83714466c6d3caad6de15036c9b
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2021-02-16 16:13:00 +0200

    Init commit

--- src/components/CreateElementComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class CreateElementComponent extends React.Component {
-  render() {
-    return React.createElement('div', { className: 'createElementComponent' }, 'Hello React from CreateElementComponent');
-  }
-}
-
-export default CreateElementComponent;
--- src/components/PureComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class PureComponent extends React.PureComponent {
-  render() {
-    return <div>Hello React from the PureComponent!</div>;
-  }
-}
-
-export default PureComponent;
