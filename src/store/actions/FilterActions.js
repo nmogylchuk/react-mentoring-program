8c42ea1066c9000887023db394ad3ff8d7c84048
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2021-04-11 22:36:52 +0300

    Add not found and no movies components, search reducers and actions

---
+++ src/store/actions/FilterActions.js
@@ -0,0 +1,6 @@
+export const SET_FILTER_VALUE = 'SET_FILTER_VALUE';
+
+export const setFilterValue = value => ({
+    type: SET_FILTER_VALUE,
+    payload: value
+});
---
+++ src/store/reducers/searchReducer.js
@@ -0,0 +1,19 @@
+import { SET_SEARCH_VALUE, SET_SEARCH_BY } from 'store/actions/SearchActions';
+
+const initialState = {
+  searchValue: '',
+  searchBy: 'title',
+};
+
+export function searchReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_SEARCH_VALUE:
+      return { ...state, searchValue: action.payload };
+    case SET_SEARCH_BY:
+      return { ...state, searchBy: action.payload };
+    default:
+      return state;
+  }
+}
+
+export default searchReducer;
